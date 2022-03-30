fbe401503fc3c50f5ef6db2530c6b2528d1ef600
soju6jan <cybersol@naver.com>
2022-03-30 11:39:37 +0900

    로그 제거

--- logic_kp_base.py
+++ logic_kp_base.py
@@ -83,7 +83,7 @@ class LogicKPBase(LogicModuleBase):
     def plugin_load(self):
         from .tool import Tool
         self.source_map = Tool.source_decrypt()
-        logger.debug(self.source_map)
+        #logger.debug(self.source_map)
 
 
     def get_ch_list(self):
