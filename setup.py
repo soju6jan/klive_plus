235c7011d78973741db69a35f0853f37c2e39587
flaskfarm <flaskfarm@gmail.com>
2022-11-10 06:23:08 +0900

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ setting = {
     'filepath' : __file__,
     'use_db': True,
     'use_default_setting': True,
-    'home_module': 'main',
+    'home_module': None,
     'menu': {
         'uri': __package__,
         'name': 'KLive+',
